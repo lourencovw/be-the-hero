dc79bc7eb1abb99cd1b5d832ebc653dfed3f8e3b
Lourenco werneck <lourencovw@gmail.com>
2020-03-28 09:22:53 -0300

    Initial Commit

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // Transforma todas as requisiçoes em formato json
+app.use(routes);
+
+app.listen(3333)
